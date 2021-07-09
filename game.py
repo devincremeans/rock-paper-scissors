6b5c9690bd1e387564770cf16cbdef8508ed082c
devincremeans <86858013+devincremeans@users.noreply.github.com>
2021-07-08 20:36:29 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,29 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+
+# Ask for a user input
+# Source: https://www.w3schools.com/python/ref_func_input.asp
+x = input("Please choose one of 'rock', 'paper', 'scissors'")
+print(x)
+
+
+
+
+# Validate user input
+
+
+
+
+# Generate computer choice
+ 
+ 
+ 
+ 
+ 
+# Determine Winner
+
+
+
